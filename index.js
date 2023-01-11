e03eb844863fe1d3ee2315157cf0b6bbf3bb7963
Arturo Pacheco <arturojesus.pc@gmail.com>
2023-01-11 10:30:52 -0400

    java a section registrar compra

--- index.js
+++ index.js
@@ -2,27 +2,43 @@ const pesoDolares = document.querySelector("#peso-gr-d");
 const precioDolares = document.querySelector("#precio-d");
 const pesoBolivares = document.querySelector("#peso-gr-b");
 const precioBolivares = document.querySelector("#precio-b");
-const totalDolares = document.querySelector("#total-d")
-const totalBolivares = document.querySelector("#total-b")
 
-function calculadora(peso, precio) {
-    return peso * precio; 
-}
+const totalDolares = document.querySelector("#total-d");
+const totalBolivares = document.querySelector("#total-b");
 
-let intervaloDolares = setInterval(() => {
+const pesoCompra = document.querySelector("#peso-compra");
+const precioCompra = document.querySelector("#precio-compra");
+const totalPagado = document.querySelector("#total-pagado");
+const buttonCompra = document.querySelector("#button-compra");
+buttonCompra.addEventListener("click", registrarCompra);
+
+const intervaloDolares = setInterval(() => {
     if(pesoDolares.value != 0){
     let resultado = calculadora(pesoDolares.value, precioDolares.value);
     console.log(resultado);
     totalDolares.innerHTML = `${resultado.toFixed(1)}$`
 }}, 1000);
 
-let intervaloBolivares = setInterval(() => {
+const intervaloBolivares = setInterval(() => {
     if(pesoBolivares.value != 0){
     let resultado = calculadora(pesoBolivares.value, precioBolivares.value);
     console.log(resultado);
     totalBolivares.innerHTML = `${resultado.toFixed(0)}bs`
 }}, 1000);
 
+function calculadora(peso, precio) {
+    return peso * precio; 
+}
+
+function registrarCompra() {
+    let peso = pesoCompra.value;
+    let precio = precioCompra.value;
+    let total = totalPagado;
+
+    if(true){
+        total.innerHTML = `${()=>{return peso * precio}}$`
+    }
+}
 
 
 
